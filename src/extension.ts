4c5e6d20b1bb1cbc05913e7348fe0eac3cccf65c
Paweł Sołtysiak <soltys@uznam.org>
2015-11-26 22:37:04 +0100

    moved from own guid implementation to node-uuid package

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'; 
-import guid  from './guid';
+import * as  uuid from 'node-uuid';
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    console.log('Congratulations, your extension "guid" is now active!');
-
     
     var disposable = vscode.commands.registerTextEditorCommand('extension.generateGuid', (textEditor, textEditorEdit) => {
         var startPosition = textEditor.selection.start
-        textEditorEdit.insert(startPosition, guid());
-
+        textEditorEdit.insert(startPosition, uuid.v1());       
     });
-
     context.subscriptions.push(disposable);
 }
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,7 @@
+
 /// <reference path="../node_modules/vscode/typings/index.d.ts" />
+/// <reference path="node-uuid/node-uuid-cjs.d.ts" />
+
+
+
+
